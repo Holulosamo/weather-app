48f682bd49ef78988dcae890c3fbc415782c3a36
Holulosamo <eliasrodriguez000@gmail.com>
2021-11-26 01:12:41 -0300

    showing app in the DOM

--- js/main.js
+++ js/main.js
@@ -10,21 +10,37 @@ const SEARCH_URL = `${API_LINK}api/location/search/?query=${query}`,
 
 async function getWeather(url) {
   try {
+    const spinner = d.createElement("div");
+    spinner.setAttribute('style', 'border: 4px solid rgba(0, 0, 0, .2); width: 60px; height: 60px; border-radius: 50%; border-left-color: #fff; animation: spin 1s linear infinite');
+    weatherContainer.appendChild(spinner);
+
+
     let weatherRes = await fetch(url),
-      weatherJson = await weatherRes.json();
+    weatherJson = await weatherRes.json();
     console.log(weatherJson);
-
+    
     if(!weatherRes.ok) throw {status: weatherRes.status, statusText: weatherRes.statusText}
-
-    let [weather0, ...weatherRest] = weatherJson.consolidated_weather;
-    console.log(weather0);
-
+    
+    let [weatherToday, ...weatherNextDays] = weatherJson.consolidated_weather;
+    console.log(weatherToday);
+    
     weatherContainer.innerHTML = `
-      <img src="https://www.metaweather.com/static/img/weather/${weather0.weather_state_abbr}.svg" alt="weatherImg" class="aside__weather-image">
-      
+      <img src="https://www.metaweather.com/static/img/weather/${
+        weatherToday.weather_state_abbr
+      }.svg" alt="weatherImg" class="aside__weather-image">
+      <h1 class="aside__weather-temp">
+        ${Math.trunc(weatherToday.the_temp)}<span class="aside__weather-span">ºC</span>
+      </h1>
+      <h2>${weatherToday.weather_state_name}</h2>
+      <div class="aside__weather-container">
+        <span>Today</span>
+        <span>•</span>
+        <span></span>
+      </div>
     `;
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    const error = err.statusText || "An error occurred while loading the data";
+    weatherContainer.innerHTML = `<p class="aside__weather-error">Error: ${err.status}, ${error}<p>`;
   }
 }
 
