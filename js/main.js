e3b30c7344505523a18ce9087b9f63cf41b2493a
Holulosamo <eliasrodriguez000@gmail.com>
2021-12-07 14:18:38 -0300

    Getting current weather and formating forecast

--- js/main.js
+++ js/main.js
@@ -4,47 +4,33 @@ const d = document,
   weatherGrid = d.querySelector(".article");
 
 let query;
-const API_LINK = "https://www.metaweather.com/";
-const SEARCH_URL = `${API_LINK}api/location/search/?query=${query}`, 
-      DEFAULT_URL = `${API_LINK}api/location/44418/`;
+const API_LINK = "https://api.openweathermap.org/",
+  DEFAULT_CURRENT_WEATHER = `${API_LINK}data/2.5/weather?q=London&appid=47215c95b17bd7ea41ea062f704ea884`,
+  DEFAULT_FORECAST = `${API_LINK}data/2.5/forecast?q=London&appid=47215c95b17bd7ea41ea062f704ea884`;
 
-async function getWeather(url) {
+async function getWeather(currentWeatherURL, forecastURL) {
   try {
     const spinner = d.createElement("div");
     spinner.setAttribute('style', 'border: 4px solid rgba(0, 0, 0, .2); width: 60px; height: 60px; border-radius: 50%; border-left-color: #fff; animation: spin 1s linear infinite');
     weatherContainer.appendChild(spinner);
 
+    let currWeatherFetch = await fetch(currentWeatherURL),
+        forecastFetch = await fetch(forecastURL),
+        [currWeatherRes, forecastRes] = await Promise.all([currWeatherFetch, forecastFetch]),
+        currWeatherJson = await currWeatherRes.json(),
+        forecastJson = await forecastRes.json();
+        
+    console.log(currWeatherJson);
 
-    let weatherRes = await fetch(url),
-    weatherJson = await weatherRes.json();
-    console.log(weatherJson);
+    const forecastFormatted = forecastJson.list.filter(forecast => forecast.dt_txt.includes("18:00:00"));
+
+    console.log(forecastFormatted)
     
-    if(!weatherRes.ok) throw {status: weatherRes.status, statusText: weatherRes.statusText}
     
-    let [weatherToday, ...weatherNextDays] = weatherJson.consolidated_weather;
-    console.log(weatherToday);
-    
-    weatherContainer.innerHTML = `
-      <img src="https://www.metaweather.com/static/img/weather/${
-        weatherToday.weather_state_abbr
-      }.svg" alt="weatherImg" class="aside__weather-image">
-      <h1 class="aside__weather-temp">
-        ${Math.trunc(weatherToday.the_temp)}<span class="aside__weather-span">ºC</span>
-      </h1>
-      <h2>${weatherToday.weather_state_name}</h2>
-      <div class="aside__weather-container">
-        <span>Today</span>
-        <span>•</span>
-        <span></span>
-      </div>
-    `;
   } catch (err) {
     const error = err.statusText || "An error occurred while loading the data";
     weatherContainer.innerHTML = `<p class="aside__weather-error">Error: ${err.status}, ${error}<p>`;
   }
 }
 
-d.addEventListener(
-  "DOMContentLoaded",
-  getWeather(DEFAULT_URL)
-);
+d.addEventListener("DOMContentLoaded", getWeather(DEFAULT_CURRENT_WEATHER, DEFAULT_FORECAST));
